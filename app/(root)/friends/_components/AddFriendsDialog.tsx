e6f885696afdb25d51a3297477c6050b72d4efea
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-06-24 21:24:35 -0300

    feat: form created to add a new friend

---
+++ app/(root)/friends/_components/AddFriendsDialog.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { UserPlus } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { api } from "@/convex/_generated/api";
+import { useMutationState } from "@/hooks/useMutationState";
+import { toast } from "sonner";
+import { ConvexError } from "convex/values";
+
+const addFriendFormSchema = z.object({
+  email: z
+    .string()
+    .min(1, { message: "E-mail é obrigatório" })
+    .email("Por favor insira um email válido"),
+});
+
+const AddFriendDialog = () => {
+  const { mutate: createRequest, pending } = useMutationState(
+    api.request.create
+  );
+
+  const form = useForm<z.infer<typeof addFriendFormSchema>>({
+    resolver: zodResolver(addFriendFormSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const handleSubmit = async (values: z.infer<typeof addFriendFormSchema>) => {
+    await createRequest({ email: values.email })
+      .then(() => {
+        form.reset();
+        toast.success("Requisição enviada com sucesso!");
+      })
+      .catch((error) => {
+        toast.error(
+          error instanceof ConvexError
+            ? error.data
+            : "Ocorreu um erro ao enviar a requisição. Por favor, tente novamente."
+        );
+      });
+  };
+
+  return (
+    <Dialog>
+      <Tooltip>
+        <TooltipTrigger>
+          <Button size="icon" variant="outline">
+            <DialogTrigger asChild>
+              <UserPlus />
+            </DialogTrigger>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Adicionar Amigo</p>
+        </TooltipContent>
+      </Tooltip>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Adicionar Amigo</DialogTitle>
+          <DialogDescription>
+            Envie uma solicitação para se conectar com o seu amigo!
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-8"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>E-mail</FormLabel>
+                  <FormControl>
+                    <Input placeholder="E-mail..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button disabled={pending} type="submit">
+                Send
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddFriendDialog;
--- app/(root)/friends/page.tsx
+++ app/(root)/friends/page.tsx
@@ -1,13 +1,16 @@
 import ConversationFallback from "@/components/shared/conversation/ConversationFallback";
 import ItemList from "@/components/shared/item-list/ItemList";
 import React from "react";
+import AddFriendsDialog from "./_components/AddFriendsDialog";
 
 type Props = {};
 
 const FriendsPage = (props: Props) => {
   return (
     <>
-      <ItemList title="Friends">Amigos</ItemList>
+      <ItemList title="Friends" action={<AddFriendsDialog />}>
+        Amigos
+      </ItemList>
       <ConversationFallback />
     </>
   );
